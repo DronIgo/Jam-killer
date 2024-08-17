578e14b56e8782fe44a37625c9c5e3bae4d420cc
Dron231 <a.igo@g.nsu.ru>
2024-08-18 02:05:18 +0700

    Перенес движение рыбы в отдельный класс
    FishAI отвечает за принятие решений, FishMover за их исполнение

--- Assets/Scripts/Fish AI/FishGoalRandomPoint.cs
+++ Assets/Scripts/Fish AI/FishGoalRandomPoint.cs
@@ -6,12 +6,14 @@ public class FishGoalRandomPoint : IFishGoal
 {
     public Vector3 goalPoint;
     public Transform fishTransform;
-    public float maxDistance = 25.0f;
-    public float minDistance = 5.0f;
+    public float maxDistance = 20.0f;
+    public float minDistance = 3.0f;
     public float goalDist = 0.1f;
+    public FishMover fishMover;
     public FishGoalRandomPoint(FishAI fish) : base(fish)
     {
         fishTransform = fish.gameObject.transform;
+        fishMover = fish.fishMover;
         float angle = Random.Range(0, Mathf.PI * 2.0f);
         float dist = Random.Range(minDistance, maxDistance);
         goalPoint = new Vector3(Mathf.Sin(angle) * dist, Mathf.Cos(angle) * dist, fishTransform.position.z);
@@ -32,7 +34,8 @@ public class FishGoalRandomPoint : IFishGoal
         Debug.DrawLine(fishTransform.position, goalPoint, Color.green, .1f, false);
         Vector3 goal = (goalPoint - fishTransform.position);
         goal.Normalize();
-        fishTransform.up = goal;
-        fishTransform.position += goal * fishAI.speed * Time.deltaTime;
+        fishMover.SetDirection(goal);
+        //fishTransform.up = goal;
+        //fishTransform.position += goal * fishAI.speed * Time.deltaTime;
     }
 }
