38ca8edd92d8a44888400371875b656d2bd28a4a
sintaxiz <natashaleind@gmail.com>
2024-08-20 04:24:56 +0700

    Add inside fish generation

--- Assets/Scripts/Environment/InsideWorldGenerator.cs
+++ Assets/Scripts/Environment/InsideWorldGenerator.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class WorldGenerator : MonoBehaviour
+public class InsideWorldGenerator : MonoBehaviour
 {
-
     public float chunkWidth = 15f;
     public float chunkHeight = 15f;
     public Vector2Int currentChunkPos;
+
+    public int wallTopEdge = 2;
+    public int wallBottomEdge = -2;
+
+    private int topEdge;
+    private int bottomEdge;
+
+    public ChunkType topWallChunk;
+    public ChunkType bottomWallChunk;
 
     public Dictionary<Vector2Int, ChunkType> generatedChunks;
     public Dictionary<Vector2Int, GameObject> existingGameObjects;
 
     public ChunkType emptyChunkType;
+
     public List<ChunkType> chunkTypes;
 
     public int generationRadius = 2;
@@ -45,9 +54,13 @@ public class WorldGenerator : MonoBehaviour
         generatedChunks = new();
         existingGameObjects = new();
 
+        topEdge = Mathf.FloorToInt(player.position.y / chunkHeight) + wallTopEdge;
+        bottomEdge = Mathf.FloorToInt(player.position.y / chunkHeight) + wallBottomEdge;
+
         // generate start chunk
         //Debug.Log("Creating first chunk...");
         UpdateCurrentChunkPos();
+
 
         GameObject chunkGameObject = PlaceChunk(currentChunkPos,emptyChunkType.prefab);
         generatedChunks.Add(currentChunkPos, emptyChunkType);
@@ -95,12 +108,31 @@ public class WorldGenerator : MonoBehaviour
         return gameObject;
     }
 
-    private void GenerateNewChunk(Vector2Int chunkPos)
+    private void GenerateNewChunk(Vector2Int chunkPos, bool random, ChunkType chunkType)
     {
-        ChunkType chunkType = GetRandomChunkType();
+        if (random)
+            chunkType = GetRandomChunkType();
+
         GameObject chunkGameObject = PlaceChunk(chunkPos, chunkType.prefab);
         generatedChunks.Add(chunkPos, chunkType);
         existingGameObjects.Add(chunkPos, chunkGameObject);
+    }
+
+    private void CreateIfNotExist(Vector2Int chunk, bool random, ChunkType chunkType)
+    {
+        if (generatedChunks.ContainsKey(chunk))
+        {
+            // if it already placed continue...
+            if (!existingGameObjects.ContainsKey(chunk))
+            {
+                // otherwise place it with correct chunkPrefab
+                GameObject chunkObject = PlaceChunk(chunk, generatedChunks[chunk].prefab);
+                existingGameObjects.Add(chunk, chunkObject);
+            }
+            return;
+        }
+
+        GenerateNewChunk(chunk, random, chunkType);
     }
 
     private void GenerateChunkArea(Vector2Int startChunk, int radius)
@@ -110,6 +142,9 @@ public class WorldGenerator : MonoBehaviour
 
         // get chunk in "radius"
         List<Vector2Int> chunks = new();
+        List<Vector2Int> topWalls = new();
+        List<Vector2Int> bottomWalls = new();
+
         for (int i = -radius; i <= radius; i++)
         {
             for (int j = -radius; j <= radius; j++)
@@ -117,6 +152,20 @@ public class WorldGenerator : MonoBehaviour
                 // skip the startChunk
                 if (i == startX && j == startY)
                     continue;
+
+                if (startY + j >= topEdge)
+                {
+                    Vector2Int wall = new(startX + i, topEdge);
+                    topWalls.Add(wall);
+                    continue;
+                }
+
+                if (startY + j <= bottomEdge)
+                {
+                    Vector2Int wall = new(startX + i, bottomEdge);
+                    bottomWalls.Add(wall);
+                    continue;
+                }
 
                 Vector2Int nearChunk = new(startX + i, startY + j);
                 chunks.Add(nearChunk);
@@ -128,21 +177,18 @@ public class WorldGenerator : MonoBehaviour
         // create nonexisting chunks
         foreach (var chunk in chunks)
         {
-            if (generatedChunks.ContainsKey(chunk))
-            {
-                // if it already placed continue...
-                if (!existingGameObjects.ContainsKey(chunk))
-                {
-                    // otherwise place it with correct chunkPrefab
-                    GameObject chunkObject = PlaceChunk(chunk, generatedChunks[chunk].prefab);
-                    existingGameObjects.Add(chunk, chunkObject);
-                }
-                continue;
-            }
-
-            GenerateNewChunk(chunk);
+            CreateIfNotExist(chunk, true, null);
         }
 
+        foreach (var wall in topWalls)
+        {
+            CreateIfNotExist(wall, false, topWallChunk);
+        }
+
+        foreach (var wall in bottomWalls)
+        {
+            CreateIfNotExist(wall, false, bottomWallChunk);
+        }
 
     }
 
